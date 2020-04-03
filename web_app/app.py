32a28c8c4ccfaecc9493882d750cfd577df48612
Domenico Pellegrino <mimmo.pellegrino89@gmail.com>
2020-04-03 11:31:17 +0100

    Created the web app

---
+++ web_app/app.py
@@ -0,0 +1,61 @@
+import os
+import json
+import numpy as np
+import pickle
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from config import Config
+from model import model_load, get_class, from_pdf_to_vector, find_similar
+
+#UPLOAD_FOLDER = 'static/pdf'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+tfidf,logr=model_load('tfidf.p','logr.p')
+database=pickle.load(open('vectorized_articles.p','rb'))
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            pdf_path=os.path.join('static/pdf',filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+    return render_template("index.html")
+
+@app.route('/uploaded/<filename>',methods=['GET'])
+def uploaded_file(filename):
+    filename=os.path.join('static/pdf',filename)
+    x,exit_status=from_pdf_to_vector(filename,tfidf)
+    if exit_status!=0:
+        flash('Sorry, it seems that something went wrong. Please try again.')
+        return redirect('/')
+    else:
+        #Get the classes that best suit the article and the corresponding probabilities
+        prob,clss=get_class(x,logr)
+        #Find the first 10 similar articles to the one uploaded in the database
+        similar_pos=find_similar(database['X'],x,10)
+        return render_template("classification.html",filename=os.path.join('/',filename),prob=prob,clss=clss,similar=database['links'][similar_pos])
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ web_app/model.py
@@ -0,0 +1,42 @@
+import pickle
+import os
+import numpy as np
+import re
+
+def clean(article):
+    document=re.sub('\[(.+?)\]','',article)
+    document=re.sub(' [0-9]+ ','',document)
+    document=re.sub('arxiv:(.*?) ','',document)
+    yield document
+
+def model_load(tfidf_path,model_path):
+    with open(tfidf_path,'rb') as file:
+        tfidf=pickle.load(file)
+    with open(model_path,'rb') as file:
+        model=pickle.load(file)
+    return tfidf,model
+
+def from_pdf_to_vector(pdf_path,tfidf):
+    if not os.path.exists('./tmp'): 
+        os.makedirs('./tmp')
+    txt_path='./tmp/article.txt'
+    cmd="pdftotext %s %s" % (pdf_path,txt_path)
+    exit=os.system(cmd)
+    with open(txt_path,'r') as file:
+        article=file.read()
+    x=tfidf.transform(clean(article))
+    os.system("rm %s"%(txt_path))
+    return x,exit
+
+def find_similar(X,x,how_many):
+    cos_similarity=X.dot(x.transpose())
+    cos_similarity=np.asarray(cos_similarity.todense()).reshape(-1)
+    return np.argsort(-cos_similarity)[:how_many+1]
+
+def get_class(x,model): 
+    prob=model.predict_proba(x)[0]
+    pos=np.argsort(-prob)
+    prob=np.dot(np.true_divide(prob[pos[:3]],np.sum(prob[pos[:3]])),100)
+    prob=prob.astype(int)
+    prob[2]=100-prob[1]-prob[0]
+    return prob,model.classes_[pos[:3]]
